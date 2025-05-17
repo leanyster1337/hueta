e862036cb13ab36f04eb94186417b277d90973fd
leanyster1337 <56219572+leanyster1337@users.noreply.github.com>
2025-05-17 10:23:04 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from aiogram.enums import ParseMode
 from aiogram.filters import Command
 from aiohttp import web
 from bs4 import BeautifulSoup
+from aiogram.client.default import DefaultBotProperties
 
 # Логгирование
 logging.basicConfig(level=logging.INFO)
@@ -19,7 +20,7 @@ WEBHOOK_PATH = f"/webhook/{BOT_TOKEN}"
 WEBHOOK_URL = f"{WEBHOOK_HOST}{WEBHOOK_PATH}"
 PORT = int(os.getenv("PORT", 10000))
 
-bot = Bot(token=BOT_TOKEN, parse_mode=ParseMode.HTML)
+bot = Bot(token=BOT_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
 dp = Dispatcher()
 
 async def search_rutor(query: str):

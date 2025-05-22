4fdb86060e81a3f3a8768e130219c2c1a2bc362c
leanyster1337 <56219572+leanyster1337@users.noreply.github.com>
2025-05-22 21:35:45 +0300

    Update search.py

--- search.py
+++ search.py
@@ -2,16 +2,16 @@ import aiohttp
 from bs4 import BeautifulSoup
 
 async def search_movie(query):
-    url = f"https://kinogo.biz/index.php?do=search&subaction=search&story={query}"
+    url = f"https://kinogo.org/index.php?do=search&subaction=search&story={query}"
     async with aiohttp.ClientSession() as session:
         async with session.get(url, headers={"User-Agent": "Mozilla/5.0"}) as resp:
             html = await resp.text()
         soup = BeautifulSoup(html, "html.parser")
         results = []
-        # kinogo иногда кладёт фильмы вот так:
-        for a in soup.select(".th-item .th-title a"):
+        # На kinogo.org фильмы обычно лежат в блоках .shortstory или .th-item
+        for a in soup.select(".shortstory-title a, .th-item .th-title a"):
             title = a.text.strip()
             link = a.get("href")
-            if title and link:
+            if title and link and link.startswith("http"):
                 results.append((title, link))
         return results

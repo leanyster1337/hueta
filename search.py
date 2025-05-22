e30ab7edf75198f84c8254a35e9665c5af39a235
leanyster1337 <56219572+leanyster1337@users.noreply.github.com>
2025-05-22 16:54:13 +0300

    Update search.py

--- search.py
+++ search.py
@@ -1,31 +1,46 @@
 import aiohttp
 from bs4 import BeautifulSoup
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 async def search_movie(query):
-    url = f"https://rutor.info/search/0/0/0/2/{query.replace(' ', '+')}"
-    headers = {"User-Agent": "Mozilla/5.0"}
-
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url, headers=headers) as resp:
-            html = await resp.text()
-
+    # Аутентификация на RuTracker
+    login_url = "https://rutracker.org/forum/login.php"
+    session = aiohttp.ClientSession()
+    
+    # Логин
+    await session.post(login_url, data={
+        "login_username": os.getenv("RUTRACKER_LOGIN"),
+        "login_password": os.getenv("RUTRACKER_PASSWORD"),
+        "login": "Вход"
+    }, headers={"User-Agent": "Mozilla/5.0"})
+    
+    # Поиск
+    url = f"https://rutracker.org/forum/tracker.php?nm={query.replace(' ', '+')}"
+    async with session.get(url) as resp:
+        html = await resp.text()
+    
     soup = BeautifulSoup(html, "html.parser")
     results = []
-
-    for row in soup.select("tr.gai, tr.gaj")[:5]:
-        link = row.select_one("a[href^='/torrent/']")
-        if not link:
+    
+    # Парсинг результатов
+    for row in soup.select("tr.tCenter.hl-tr")[:5]:
+        title_elem = row.select_one("a.tLink")
+        if not title_elem:
             continue
-        title = link.text.strip()
-        torrent_url = "https://rutor.info" + link["href"]
-
-        # Переход по ссылке и поиск magnet-ссылки
-        async with aiohttp.ClientSession() as session:
-            async with session.get(torrent_url, headers=headers) as page:
-                page_html = await page.text()
-                page_soup = BeautifulSoup(page_html, "html.parser")
-                magnet_link = page_soup.find("a", href=True, string="Magnet")
-                if magnet_link:
-                    results.append((title, magnet_link["href"]))
-
+            
+        title = title_elem.text.strip()
+        torrent_url = "https://rutracker.org/forum/" + title_elem["href"]
+        
+        # Получение magnet-ссылки
+        async with session.get(torrent_url) as page:
+            page_html = await page.text()
+            page_soup = BeautifulSoup(page_html, "html.parser")
+            magnet = page_soup.find("a", {"class": "magnet-link"})
+            if magnet:
+                results.append((title, magnet["href"]))
+    
+    await session.close()
     return results
